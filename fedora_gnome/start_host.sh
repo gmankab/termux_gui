ca8437d2365e31a10de9a46bdc93f34315c4021f
imneokitty <rachlinups@gmail.com>
2024-07-28 15:20:01 +0300

    Add PulseAudio support

--- fedora_gnome/start_host.sh
+++ fedora_gnome/start_host.sh
@@ -1,6 +1,5 @@
 kill -9 $(pgrep -f "termux.x11") 2>/dev/null
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 termux-x11 :0 >/dev/null &
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity 2>/dev/null
 proot-distro login fedora --shared-tmp -- /bin/bash ~/termux_gui/fedora_gnome/start_guest.sh
