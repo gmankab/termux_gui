69f750444d93fe3fcd51af7433e0850f8d474bc6
imneokittyy <rachlinups@gmail.com>
2024-07-28 08:58:10 +0300

    Switch to en_US.UTF-8 locale

--- fedora_gnome/start_host.sh
+++ fedora_gnome/start_host.sh
@@ -1,6 +1,6 @@
 kill -9 $(pgrep -f "termux.x11") 2>/dev/null
-export LANG=C
-export LC_ALL=C
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 termux-x11 :0 >/dev/null &
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity 2>/dev/null
 proot-distro login fedora --shared-tmp -- /bin/bash ~/termux_gui/fedora_gnome/start_guest.sh
