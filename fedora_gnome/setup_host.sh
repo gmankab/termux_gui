4e9d5a64746009381a7139326f1e618503d26d43
gmanka <gmankab@gmail.com>
2024-07-28 13:08:14 +0300

    [u] container name, install command

--- fedora_gnome/setup_host.sh
+++ fedora_gnome/setup_host.sh
@@ -2,6 +2,6 @@ pkg upgrade -y
 pkg install x11-repo
 pkg install termux-x11-nightly pulseaudio proot-distro
 termux-setup-storage
-proot-distro install fedora
-proot-distro login fedora -- bash ~/termux_gui/fedora_gnome/setup_guest.sh
+proot-distro install fedora --override-alias fedora_gnome
+proot-distro login fedora_gnome -- bash ~/termux_gui/fedora_gnome/setup_guest.sh
 
