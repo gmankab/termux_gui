fa8a2d518852a747014ab718af594e61f5629a37
imneokittyy <rachlinups@gmail.com>
2024-07-28 15:11:39 +0300

    Update setup_host.sh

--- fedora_gnome/setup_host.sh
+++ fedora_gnome/setup_host.sh
@@ -1,6 +1,7 @@
-pkg upgrade -y
-pkg install x11-repo
-pkg install termux-x11-nightly pulseaudio proot-distro
+pkg update && pkg upgrade -y
+pkg install -y x11-repo
+pkg install -y termux-x11-nightly pulseaudio proot-distro
+#pkg install -y mesa-zink virglrenderer-mesa-zink 
 termux-setup-storage
 proot-distro install fedora
 proot-distro login fedora -- bash ~/termux_gui/fedora_gnome/setup_guest.sh
